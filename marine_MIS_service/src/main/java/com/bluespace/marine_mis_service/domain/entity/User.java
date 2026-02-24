b0a5080312ee0e69ffae5d712585fef1a8aee0cc
backduu <power4290@gmail.com>
2026-02-24 09:22:02 +0900

    [수정] UserDetails 인터페이스 내 필수 메서드 구현

--- marine_MIS_service/src/main/java/com/bluespace/marine_mis_service/domain/entity/User.java
+++ marine_MIS_service/src/main/java/com/bluespace/marine_mis_service/domain/entity/User.java
@@ -5,19 +5,26 @@ import com.bluespace.marine_mis_service.domain.enums.UserStatus;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * <pre>
  * ===========================================================
- * Program Name : WelcomeController
- * Description  : 환영/헬스 체크 API
- * Author       : <YOUR_NAME>
- * Create Date  : 2026-02-11
+ * Program Name : User.java
+ * Description  : 사용자 엔티티
+ * Author       : 백두현
+ * Create Date  : 2026-02-23
  *
  * 변경이력
  * -----------------------------------------------------------
- * 2026-02-11  <YOUR_NAME>  최초작성
+ * 2026-02-23  백두현  최초작성
+ * 2026-02-24  백두현  UserDetails 인터페이스 상속 후 필수 페서드 구현
  * ===========================================================
  * </pre>
  */
@@ -61,4 +68,75 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserStatus status;
+
+    // TODO BaseTimeEntity로 공통 시간 필드 분리 예정, auditing은 안해주기에 서비스 단에서 업데이트
+    private LocalDateTime lastLogin;
+    private LocalDateTime deleted;
+    private LocalDateTime approvedAt;
+
+    @CreatedDate
+    private LocalDateTime created;
+    @LastModifiedDate
+    private LocalDateTime updated;
+
+    // === 비즈니스 로직 === //
+    public void approved() {
+        this.status = UserStatus.ACTIVE;
+        this.approvedAt = LocalDateTime.now();
+    }
+
+    public void rejected() {
+        this.status = UserStatus.SUSPENDED;
+        this.approvedAt = LocalDateTime.now();
+    }
+
+    public void deactivated() {
+        this.status = UserStatus.INACTIVE;
+        this.approvedAt = LocalDateTime.now();
+    }
+
+    public void deleted() {
+        this.status = UserStatus.DELETED;
+        this.approvedAt = LocalDateTime.now();
+    }
+
+    public void pending() {
+        this.status = UserStatus.PENDING;
+        this.approvedAt = LocalDateTime.now();
+    }
+
+    public void blocked() {
+        this.status = UserStatus.BLOCKED;
+        this.approvedAt = LocalDateTime.now();
+    }
+
+    // TODO 사용자 마지막 로그인 1년 후 자동 비활성화시키기
+    public void updateLastLoginTime() {
+        this.lastLogin = LocalDateTime.now();
+    }
+
+    public void changeUsername(String newUsername) {
+        if (newUsername != null && !newUsername.isBlank()) {
+            this.username = newUsername;
+        }
+    }
+
+    public void changeNickname(String newNickname) {
+        if (newNickname != null && !newNickname.isBlank()) {
+            this.nickname = newNickname;
+        }
+    }
+
+    public void changePassword(String newEncodedPassword) {
+        this.password = newEncodedPassword;
+    }
+
+    // === UserDetails 구현 메소드 === //
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isEnabled() { return status == UserStatus.ACTIVE; }
+
 }
